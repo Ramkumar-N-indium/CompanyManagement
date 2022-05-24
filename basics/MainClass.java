7e3c922181c00d567929d6c9742e2d6572d97126
kamaliraja <106136963+kamaliraja@users.noreply.github.com>
2022-05-24 17:22:06 +0530

    Update MainClass.java

--- basics/MainClass.java
+++ basics/MainClass.java
@@ -1,11 +1,11 @@
 package basics;
-
+  // class mainclass
 public class MainClass {
-
+// main 
 	public static void main(String[] args) {
 		Employee emp1=new Employee("D112", "Tom", 20, 16000);
 		Employee emp2=new Employee("D113", "Jack", 20, 17000);
-		emp1.empDetails();
+		emp1.empDetails();     
 		emp2.empDetails();
 	}
 	
